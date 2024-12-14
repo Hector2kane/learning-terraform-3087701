0d198290af08a831be1b87f3c62e1692d085c8f5
Hector2kane <hector2kane@gmail.com>
2024-12-14 10:48:19 +0100

    create new dev

--- main.tf
+++ main.tf
@@ -49,10 +49,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name    = "blog_new"
 
   vpc_id               = module.blog_vpc.vpc_id
- 
+  name    = "blog_new"
+
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"] 
 
