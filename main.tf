9b4649eab041ac0017a5e669bf102d0866dca494
Hector2kane <hector2kane@gmail.com>
2024-12-16 23:23:14 +0100

    target group replaced with solution file

--- main.tf
+++ main.tf
@@ -39,14 +39,14 @@ module "autoscaling" {
   version = "8.0.0"
   
   name     = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -96,15 +96,14 @@ module "blog_alb" {
 
   }
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
