7bdd720b1511bc384c846ab9a7e44a8fbf6d6e14
Hector2kane <hector2kane@gmail.com>
2024-12-16 23:10:32 +0100

    updatae

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -60,7 +61,6 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  # Security Group
   security_group_ingress_rules = {
     all_http = {
       from_port   = 80
@@ -103,6 +103,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id        = aws_instance.blog.id
     }
   }
 
