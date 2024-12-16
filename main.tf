9b9d8db1707c41a274097b594ea48c1bcb50e9ef
Hector2kane <hector2kane@gmail.com>
2024-12-16 23:12:21 +0100

    target update

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_id        = module.blog_alb.target_group_arns
     }
   }
 
