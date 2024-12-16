39c0ec77a8291061730930f5a789383f4380ca4d
Hector2kane <hector2kane@gmail.com>
2024-12-16 23:15:59 +0100

    target aws instance added

--- main.tf
+++ main.tf
@@ -43,7 +43,6 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -103,7 +102,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = module.blog_alb.target_group_arns
+      target_id        = aws_instance.blog.id
     }
   }
 
