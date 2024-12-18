a23c9c6159b58630de78897d43ed187f2ffcde46
Hector2kane <hector2kane@gmail.com>
2024-12-18 15:29:10 +0100

    copy from solution file

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner]
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -21,7 +22,8 @@ module "blog_vpc" {
   cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["${var.environment.network_prefix}.0.101.0/24", "${var.environment.network_prefix}.0.102.0/24", "${var.environment.network_prefix}.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
     Terraform = "true"
@@ -29,27 +31,27 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.0.0"
 
-  name      = "${var.environment.name}-blog"
-  min_size  = var.asg_min_size
-  max_size  = var.asg_max_size
+  name = "blog"
 
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  #90target_group_arns   = [module.blog_alb.target_group_arns]
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}-blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
@@ -59,14 +61,14 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
     }
   ]
 
-  http_tcp_listeners     = [
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
@@ -74,8 +76,8 @@ module "blog_alb" {
     }
   ]
 
-  tags          = {
-    Environment = var.environment.name
+  tags = {
+    Environment = "dev"
   }
 }
 
@@ -83,13 +85,10 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
- 
-  name    = "${var.environment.name}-blog"
   vpc_id  = module.blog_vpc.vpc_id
-  
-  ingress_rules       = ["https-443-tcp","http-80-tcp"]
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
