2ee12bfd6de301faa4aae31612dd42e225acdd0f
Hector2kane <hector2kane@gmail.com>
2024-12-18 18:10:15 +0100

    copy from solution file2

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
-    name   = "blog-alb"
+    name   = "name"
     values = [var.ami_filter.name]
   }
 
@@ -11,38 +11,37 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner] # Bitnami
+  owners = [var.ami_filter.owner]
 }
-
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
-
-
+  enable_nat_gateway = true
+  
   tags = {
     Terraform = "true"
     Environment = var.environment.name
   }
 }
 
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.0.0"
 
   name = "blog"
-
+  
   min_size            = var.asg_min
   max_size            = var.asg_max
+
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  #90target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
+
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
 }
@@ -52,7 +51,6 @@ module "blog_alb" {
   version = "~> 6.0"
 
   name = "blog-alb"
-
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
@@ -75,7 +73,6 @@ module "blog_alb" {
       target_group_index = 0
     }
   ]
-
   tags = {
     Environment = "dev"
   }
@@ -87,6 +84,7 @@ module "blog_sg" {
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
+
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
